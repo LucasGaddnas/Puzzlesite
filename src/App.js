c8fb802debb1a99b9241060230c3152084ded3f2
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-10-01 14:26:24 +0300

    Add components for header, footer and uploadform

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import UploadForm from './components/UploadForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <UploadForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h4>&copy;Lucas Gäddnäs</h4>
+        </div>
+    )
+}
+
+export default Footer;
