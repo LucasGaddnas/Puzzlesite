8c8305f8b8ff4baa82cfbeb48b0c9e2d79e917a5
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-10-01 14:54:18 +0300

    Create an effect hook for uploading files

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const fileTypes = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let uploadedFile = e.target.files[0];
+
+        if (uploadedFile && fileTypes.includes(uploadedFile.type)) {
+            setFile(uploadedFile);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please select a png or jpg');
+        }
+    }
+
     return (
         <form>
-            <input type="file"/>
+            <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                { error && <div className="error">{ error }</div> }
+                { file && <div>{ file.name }</div> }
+            </div>
         </form>
     )
 }
