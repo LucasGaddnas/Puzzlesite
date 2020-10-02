86c69673584ca735655a3def415a314040ab2e22
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-10-02 19:20:12 +0300

    Add modal for highlighting an image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 import UploadForm from './components/UploadForm';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Header />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/> }
       <Footer />
     </div>
   );
