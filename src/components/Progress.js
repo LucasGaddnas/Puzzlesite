9fea87c097c614e07cd852e563ad8a1e555f121f
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-10-03 10:55:47 +0300

    Add Framer-motion and some basic animations

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
 const Progress = ({ file, setFile }) => {
     const { url, progress } = useStorage(file);
@@ -11,7 +12,9 @@ const Progress = ({ file, setFile }) => {
     }, [url, setFile]);
 
     return (
-        <div className="progress-bar" style={{width: progress + '%'}}></div>
+        <motion.div className="progress-bar"
+        initial={{ width: 0 }}
+        animate={{ width: progress + '%' }} ></motion.div>
     )
 }
 
